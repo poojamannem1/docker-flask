e579a6cabba10591b0ec8cd14a17ab9e5a65f959
poojamannem1 <poojamannem1.com>
2025-12-08 17:46:24 +0900

    test1 commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello customers!"
+    return "Hello customers! do you need anything"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
